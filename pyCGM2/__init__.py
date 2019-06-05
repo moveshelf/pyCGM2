fd8963aa7fe9023c270aef15eebd4213dfd72ccc
Allard Hoeve <allardhoeve@gmail.com>
2019-06-05 17:15:36 +0200

    Allow import of pyCMG2 on python3.7 on Linux

--- pyCGM2/__init__.py
+++ pyCGM2/__init__.py
@@ -4,58 +4,69 @@ import re
 import os
 import logging
 
+
 def getLastNexusVersion():
-    nexusDir = "C:\Program Files (x86)\Vicon"
+    nexusDir = "C:/Program Files (x86)/Vicon"
     dirs = os.listdir(nexusDir)
-    li =[]
+    li = []
     for it in dirs:
         if "Nexus2" in it:
             version = int(it[it.find(".")+1:])
             li.append(version)
     last = max(li)
-    return "Nexus2."+str(last)
+    return "Nexus2." + str(last)
+
 
 try:
     NEXUS_VERSION = getLastNexusVersion()
-    if not "C:/Program Files (x86)/Vicon/"+NEXUS_VERSION+"/SDK/Python" in sys.path:
-        sys.path.append( "C:/Program Files (x86)/Vicon/"+NEXUS_VERSION+"/SDK/Python")
+except OSError as e:
+    logging.error("No Vicon software installed: " + str(e))
+    NEXUS_VERSION = None
 
-    if not "C:/Program Files (x86)/Vicon/Nexus"+NEXUS_VERSION+"/SDK/Win32" in sys.path:
-        sys.path.append( "C:/Program Files (x86)/Vicon/"+NEXUS_VERSION+"/SDK/Win32")
-except Exception, errormsg:
-    logging.error (errormsg)
+
+if NEXUS_VERSION:
+    NEXUS_PATHS = ("C:/Program Files (x86)/Vicon/" + NEXUS_VERSION + "/SDK/Python",
+                   "C:/Program Files (x86)/Vicon/Nexus" + NEXUS_VERSION + "/SDK/Win32")
+
+    valid_paths = [path for path in NEXUS_PATHS if os.path.exists(path)]
+
+    if not valid_paths:
+        logging.error("Could not find the Vicon SDK in known directories: " + str(list(NEXUS_PATHS)))
+
+    for path in valid_paths:
+        if path not in sys.path:
+            sys.path.append(sys.path)
+
 
 # CONSTANTS
 cased_path = glob.glob(re.sub(r'([^:])(?=[/\\]|$)', r'[\1]', __file__))[0]
-MAIN_PYCGM2_PATH = os.path.abspath(os.path.join(os.path.dirname(cased_path), os.pardir)) + "\\"
+MAIN_PYCGM2_PATH = os.path.abspath(os.path.join(os.path.dirname(cased_path), os.pardir))
 
 #  [Optional] setting folder
-PYCGM2_SETTINGS_FOLDER = MAIN_PYCGM2_PATH+"pyCGM2\Settings\\"
+PYCGM2_SETTINGS_FOLDER = os.path.join(MAIN_PYCGM2_PATH, "pyCGM2", "Settings")
 
 
 #  [Optional]programData
-if  os.path.isdir(os.getenv("PROGRAMDATA")+"\\pyCGM2"):
-    PYCGM2_APPDATA_PATH = os.getenv("PROGRAMDATA")+"\\pyCGM2\\"
+if "PROGRAMDATA" in os.environ and os.path.isdir(os.path.join(os.environ["PROGRAMDATA"], "pyCGM2")):
+    PYCGM2_APPDATA_PATH = os.path.join(os.environ["PROGRAMDATA"], "pyCGM2")
 else:
     PYCGM2_APPDATA_PATH = PYCGM2_SETTINGS_FOLDER
 
 
-
-
 # [Optional]: Apps path
-MAIN_PYCGM2_APPS_PATH = MAIN_PYCGM2_PATH+"Apps\\"
+MAIN_PYCGM2_APPS_PATH = os.path.join(MAIN_PYCGM2_PATH, "Apps")
 
 # [Optional] path to embbbed Normative data base.
-NORMATIVE_DATABASE_PATH = MAIN_PYCGM2_PATH +"pyCGM2\\Data\\normativeData\\"  # By default, use pyCGM2-embedded normative data ( Schartz - Pinzone )
+# By default, use pyCGM2-embedded normative data ( Schartz - Pinzone )
+NORMATIVE_DATABASE_PATH = os.path.join(MAIN_PYCGM2_PATH, "pyCGM2", "Data", "normativeData")
 
 # [Optional] main folder containing osim model
-OPENSIM_PREBUILD_MODEL_PATH = PYCGM2_APPDATA_PATH + "opensim\\"
+OPENSIM_PREBUILD_MODEL_PATH = os.path.join(PYCGM2_APPDATA_PATH, "opensim")
 
 # [Optional] path pointing at Data Folders used for Tests
 TEST_DATA_PATH = "C:\\Users\\HLS501\\Documents\\VICON DATA\\pyCGM2-Data\\"
 MAIN_BENCHMARK_PATH = "C:\\Users\\HLS501\\Documents\\VICON DATA\\pyCGM2-benchmarks\\Gait patterns\\"
 
 # [optional] path pointing pyCGM2-Nexus tools
-NEXUS_PYCGM2_TOOLS_PATH = MAIN_PYCGM2_PATH + "pyCGM2\\Nexus\\"
-
-PYCGM2_SCRIPTS_PATH = MAIN_PYCGM2_PATH+"Scripts\\"
+NEXUS_PYCGM2_TOOLS_PATH = os.path.join(MAIN_PYCGM2_PATH, "pyCGM2", "Nexus")
+PYCGM2_SCRIPTS_PATH = os.path.join(MAIN_PYCGM2_PATH, "Scripts")
